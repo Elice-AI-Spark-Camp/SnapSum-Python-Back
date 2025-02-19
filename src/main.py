423f5e3e3b9e3db68441b453e97dfb460378f3c8
YachaTree <dnftksghkfkd@icloud.com>
2025-02-19 17:04:39 +0900

    Fix: flask에서 fastapi로 전환(main.py)

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
-from flask import Flask
-from routes.crawling_routes import crawling_bp
+# from flask import Flask
+# from routes.crawling_routes import crawling_bp
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-app.register_blueprint(crawling_bp, url_prefix="/crawl")
+# app.register_blueprint(crawling_bp, url_prefix="/crawl")
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5001, debug=True)
+
+from fastapi import FastAPI
+from routes.crawling_routes import router as crawling_router
+
+app = FastAPI()
+
+# 크롤링 API 라우트 등록
+app.include_router(crawling_router, prefix="/crawl")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5001, reload=True)
+
